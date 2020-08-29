28c60a5d66de1ee7b4a48f9ff56f0518b267eef3
강현승 <khskeb0513@gmail.com>
2020-08-29 23:06:20 +0900

    공통 브라우저 창 이용하도록 수정

--- routes/selfDig.js
+++ routes/selfDig.js
@@ -3,77 +3,84 @@ const express = require('express');
 const router = express.Router();
 const uuid = require('uuid').v4
 
+const browser = puppeteer.launch({
+    headless: false,
+    args: ['--ignore-certificate-errors'],
+    slowMo: '250ms'
+});
+let chain = Promise.resolve();
+
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    (async () => {
-        let firstCount = 0
-        let secondCount = 0
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--ignore-certificate-errors',
-                '--no-sandbox']
-        });
-        const page = await browser.newPage();
-        await page.goto('https://eduro.pen.go.kr/stv_cvd_co00_002.do', {waitUntil: "networkidle2"});
-        await page.type('input#schulNm', '부산고등학교')
-        await page.type('input#pName', '강현승')
-        await page.keyboard.press('Tab')
-        await page.type('input#frnoRidno', '020513')
-        while (page.url() === 'https://eduro.pen.go.kr/stv_cvd_co00_002.do') {
-            // firstCount++
-            // if (firstCount > 1000) {
-            //     res.send('error')
-            //     throw console.log('too many tries')
-            // }
-            try {
-                await page.click('button#btnConfirm')
-                await page.on('dialog', async dialog => {
-                    console.log(dialog.message());
-                    await dialog.dismiss();
-                    await browser.close();
-                });
-            } catch (e) {
-                if (page.url() !== 'https://eduro.pen.go.kr/stv_cvd_co00_000.do') {
-                    res.send('error')
-                    throw console.error(e)
+router.get('/', async function (req, res, next) {
+    let firstCount = 0
+    let secondCount = 0
+    const schoolName = req.query['school']
+    const Name = req.query['name']
+    const Birth = req.query['birth']
+    chain = chain.then(async () => {
+        browser.then(async r => {
+            const page = await r.newPage()
+            await page.goto('https://eduro.pen.go.kr/stv_cvd_co00_002.do', {waitUntil: "networkidle2"});
+            await page.type('input#schulNm', schoolName)
+            await page.type('input#pName', Name)
+            await page.keyboard.press('Tab')
+            await page.type('input#frnoRidno', Birth)
+            while (page.url() === 'https://eduro.pen.go.kr/stv_cvd_co00_002.do') {
+                // firstCount++
+                // if (firstCount > 1000) {
+                //     res.send('에러발생, F5 및 위로 끌어당겨 새로고침 바람.')
+                //     throw console.log('too many tries')
+                // }
+                try {
+                    await page.click('button#btnConfirm')
+                } catch (e) {
+                    if (page.url() !== 'https://eduro.pen.go.kr/stv_cvd_co00_000.do') {
+                        res.send('에러발생, F5 및 위로 끌어당겨 새로고침 바람.')
+                        throw console.error(e)
+                    }
                 }
+                // await page.on('dialog', async dialog => {
+                //     console.log(dialog.message());
+                //     await dialog.dismiss();
+                //     await browser.close();
+                // });
             }
-        }
-        await page.click('input#rspns011')
-        await page.click('input#rspns02')
-        await page.click('input#rspns070')
-        await page.click('input#rspns080')
-        await page.click('input#rspns090')
-        while (page.url() === 'https://eduro.pen.go.kr/stv_cvd_co00_000.do') {
-            // secondCount++
-            // if (secondCount > 1000) {
-            //     res.send('error')
-            //     throw console.log('too many tries')
-            // }
-            try {
-                await page.click('button#btnConfirm')
-                await page.on('dialog', async dialog => {
-                    console.log(dialog.message());
-                    await dialog.dismiss();
-                    await browser.close();
-                });
-            } catch (e) {
-                if (page.url() !== 'https://eduro.pen.go.kr/stv_cvd_co02_000.do') {
-                    res.send('error')
-                    throw console.error(e)
+            await page.click('input#rspns011')
+            await page.click('input#rspns02')
+            await page.click('input#rspns070')
+            await page.click('input#rspns080')
+            await page.click('input#rspns090')
+            while (page.url() === 'https://eduro.pen.go.kr/stv_cvd_co00_000.do') {
+                // secondCount++
+                // if (secondCount > 1000) {
+                //     res.send('에러발생, F5 및 위로 끌어당겨 새로고침 바람.')
+                //     throw console.log('too many tries')
+                // }
+                try {
+                    await page.click('button#btnConfirm')
+                } catch (e) {
+                    if (page.url() !== 'https://eduro.pen.go.kr/stv_cvd_co02_000.do') {
+                        res.send('에러발생, F5 및 위로 끌어당겨 새로고침 바람.')
+                        throw console.error(e)
+                    }
                 }
+                // await page.on('dialog', async dialog => {
+                //     console.log(dialog.message());
+                //     await dialog.dismiss();
+                //     await browser.close();
+                // });
             }
-        }
-        const token = uuid()
-        await page.screenshot({
-            path: `./public/images/results/${token}.png`
-        }).then(r => {
-            res.render('self', {
-                token: token + '.png'
+            const token = uuid()
+            await page.screenshot({
+                path: `./public/images/results/${token}.png`
+            }).then(r => {
+                res.render('self', {
+                    token: token + '.png',
+                })
             })
+            await page.close();
         })
-        await browser.close();
-    })();
+    })
 });
 
 module.exports = router;
